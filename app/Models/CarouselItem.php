e1c168cb566ee982d0cd768a0fd718526c4f16ac
julies20 <juliesshinlavistre@gmail.coml>
2023-10-15 16:22:53 +0800

    Added Migration File and Model for Carousel

---
+++ app/Models/CarouselItem.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CarouselItem extends Model
+{
+    use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'carousel_item';
+
+    
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'corousel_item_id'
+
+    
+    protected $fillable - [
+       'carousel_item',
+       'image_path', 
+       'description',
+       'user_id',
+    ]
+}
